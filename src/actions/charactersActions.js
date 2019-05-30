92f8333d6f736f7ae0f498dc3a41751eab7f13cf
Laura Badgley <l.badgleyy@gmail.com>
2019-05-30 14:06:22 -0700

    refactor with promiseMiddleware version 1 rendering characters

--- src/actions/charactersActions.js
+++ src/actions/charactersActions.js
@@ -1,25 +1,12 @@
+import { createAction } from 'promise-middleware-redux';
 import { getCharacters } from '../services/LastAirbenderApi';
 
 export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
 export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
 export const FETCH_CHARACTERS_ERROR = 'FETCH_CHARACTERS_ERROR';
 
-export const fetchCharacters = () => dispatch => {
-  dispatch({
-    type: FETCH_CHARACTERS_LOADING
-  });
-
-  return getCharacters()
-    .then(character => {
-      dispatch({
-        type: FETCH_CHARACTERS,
-        payload: character
-      });
-    })
-    .catch(err => {
-      dispatch({
-        type: FETCH_CHARACTERS_ERROR,
-        payload: err
-      });
-    });
-};
+export const fetchCharacters = () => ({
+  type: FETCH_CHARACTERS,
+  pendingType: FETCH_CHARACTERS_LOADING,
+  payload: getCharacters()
+});
